848309dcff2df6f0dd06b159baef3c596914b257
Rupendra22 <rupendra.aathiya@marutitech.com>
2023-04-19 12:27:34 +0530

    made netlify changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,108 +21,81 @@ module.exports = {
     },
   },
   plugins: [
-    `gatsby-plugin-image`,
+ 
+    "gatsby-plugin-react-helmet",
+  
+ 
+
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-plugin-sharp`,
       options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
+        defaults: {
+          quality: 100,
+          backgroundColor: `transparent`,
+          placeholder: "none"
+        }
+      }
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 630,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    // `gatsby-plugin-client-side-redirect`,
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
     `gatsby-plugin-netlify`,
  
     {
-      resolve: `gatsby-plugin-feed`,
+      resolve: "gatsby-transformer-remark",
       options: {
-        query: `
+        plugins: [
           {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
+            resolve: "gatsby-remark-images-contentful",
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 800,
+              linkImagesToOriginal: false
             }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.nodes.map(node => {
-                return Object.assign({}, node.frontmatter, {
-                  description: node.excerpt,
-                  date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
-                  custom_elements: [{ "content:encoded": node.html }],
-                })
-              })
-            },
-            query: `{
-              allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
-                nodes {
-                  excerpt
-                  html
-                  fields {
-                    slug
-                  }
-                  frontmatter {
-                    title
-                    date
-                  }
-                }
-              }
-            }`,
-            output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
           },
-        ],
-      },
+          {
+            resolve: "gatsby-remark-table-of-contents",
+            options: {
+              exclude: "Table of Contents",
+              tight: false,
+              ordered: false,
+              fromHeading: 1,
+              toHeading: 6,
+              className: "table-of-contents"
+            }
+          },
+          {
+            resolve: "gatsby-remark-autolink-headers",
+            options: {
+              offsetY: 100,
+              maintainCase: false,
+              removeAccents: false
+            }
+          },
+         
+        ]
+      }
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+  
+ 
+    // {
+    //   resolve: "gatsby-plugin-gatsby-cloud",
+    //   options: {
+    //     headers: {
+    //       "/*": ["X-Frame-Options: SAMEORIGIN"]
+    //     }, // option to add more headers. `Link` headers are transformed by the below criteria
+    //     // allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+    //     // mergeSecurityHeaders: true, // boolean to turn off the default security headers
+    //     // mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+    //     mergeCachingHeaders: false // boolean to turn off the default caching headers
+    //     // transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+    //     // generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+    //   }
+    // },
+
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,62 +4,62 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
 
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+// const path = require(`path`)
+// const { createFilePath } = require(`gatsby-source-filesystem`)
 
 // Define the template for blog post
-const blogPost = path.resolve(`./src/templates/blog-post.js`)
+// const blogPost = path.resolve(`./src/templates/blog-post.js`)
 
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
 exports.createPages = async ({ graphql, actions, reporter }) => {
-  const { createPage } = actions
+//   const { createPage } = actions
 
-  // Get all markdown blog posts sorted by date
-  const result = await graphql(`
-    {
-      allMarkdownRemark(sort: { frontmatter: { date: ASC } }, limit: 1000) {
-        nodes {
-          id
-          fields {
-            slug
-          }
-        }
-      }
-    }
-  `)
+//   // Get all markdown blog posts sorted by date
+//   const result = await graphql(`
+//     {
+//       allMarkdownRemark(sort: { frontmatter: { date: ASC } }, limit: 1000) {
+//         nodes {
+//           id
+//           fields {
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  if (result.errors) {
-    reporter.panicOnBuild(
-      `There was an error loading your blog posts`,
-      result.errors
-    )
-    return
-  }
+  // if (result.errors) {
+  //   reporter.panicOnBuild(
+  //     `There was an error loading your blog posts`,
+  //     result.errors
+  //   )
+  //   return
+  // }
 
-  const posts = result.data.allMarkdownRemark.nodes
+  // const posts = result.data.allMarkdownRemark.nodes
 
   // Create blog posts pages
   // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
   // `context` is available in the template as a prop and as a variable in GraphQL
 
-  if (posts.length > 0) {
-    posts.forEach((post, index) => {
-      const previousPostId = index === 0 ? null : posts[index - 1].id
-      const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
+  // if (posts.length > 0) {
+  //   posts.forEach((post, index) => {
+  //     const previousPostId = index === 0 ? null : posts[index - 1].id
+  //     const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
 
-      createPage({
-        path: post.fields.slug,
-        component: blogPost,
-        context: {
-          id: post.id,
-          previousPostId,
-          nextPostId,
-        },
-      })
-    })
-  }
+  //     createPage({
+  //       path: post.fields.slug,
+  //       component: blogPost,
+  //       context: {
+  //         id: post.id,
+  //         previousPostId,
+  //         nextPostId,
+  //       },
+  //     })
+  //   })
+  // }
   const {createRedirect} = actions 
   createRedirect({  fromPath: `/*`,
   toPath: `/`,
@@ -70,61 +70,61 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 /**
  * @type {import('gatsby').GatsbyNode['onCreateNode']}
  */
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+// exports.onCreateNode = ({ node, actions, getNode }) => {
+//   const { createNodeField } = actions
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const value = createFilePath({ node, getNode })
 
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
+//     createNodeField({
+//       name: `slug`,
+//       node,
+//       value,
+//     })
+//   }
+// }
 
 /**
  * @type {import('gatsby').GatsbyNode['createSchemaCustomization']}
  */
-exports.createSchemaCustomization = ({ actions }) => {
-  const { createTypes } = actions
+// exports.createSchemaCustomization = ({ actions }) => {
+//   const { createTypes } = actions
 
-  // Explicitly define the siteMetadata {} object
-  // This way those will always be defined even if removed from gatsby-config.js
+//   // Explicitly define the siteMetadata {} object
+//   // This way those will always be defined even if removed from gatsby-config.js
 
-  // Also explicitly define the Markdown frontmatter
-  // This way the "MarkdownRemark" queries will return `null` even when no
-  // blog posts are stored inside "content/blog" instead of returning an error
-  createTypes(`
-    type SiteSiteMetadata {
-      author: Author
-      siteUrl: String
-      social: Social
-    }
+//   // Also explicitly define the Markdown frontmatter
+//   // This way the "MarkdownRemark" queries will return `null` even when no
+//   // blog posts are stored inside "content/blog" instead of returning an error
+//   createTypes(`
+//     type SiteSiteMetadata {
+//       author: Author
+//       siteUrl: String
+//       social: Social
+//     }
 
-    type Author {
-      name: String
-      summary: String
-    }
+//     type Author {
+//       name: String
+//       summary: String
+//     }
 
-    type Social {
-      twitter: String
-    }
+//     type Social {
+//       twitter: String
+//     }
 
-    type MarkdownRemark implements Node {
-      frontmatter: Frontmatter
-      fields: Fields
-    }
+//     type MarkdownRemark implements Node {
+//       frontmatter: Frontmatter
+//       fields: Fields
+//     }
 
-    type Frontmatter {
-      title: String
-      description: String
-      date: Date @dateformat
-    }
+//     type Frontmatter {
+//       title: String
+//       description: String
+//       date: Date @dateformat
+//     }
 
-    type Fields {
-      slug: String
-    }
-  `)
-}
+//     type Fields {
+//       slug: String
+//     }
+//   `)
+// }
