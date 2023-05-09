816d21c5014563b5508d42eae5313c0964243b08
Rupendra22 <rupendra.aathiya@marutitech.com>
2023-05-09 12:43:19 +0530

    404 page test

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,52 +14,8 @@
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
 exports.createPages = async ({ graphql, actions, reporter }) => {
-//   const { createPage } = actions
 
-//   // Get all markdown blog posts sorted by date
-//   const result = await graphql(`
-//     {
-//       allMarkdownRemark(sort: { frontmatter: { date: ASC } }, limit: 1000) {
-//         nodes {
-//           id
-//           fields {
-//             slug
-//           }
-//         }
-//       }
-//     }
-//   `)
-
-  // if (result.errors) {
-  //   reporter.panicOnBuild(
-  //     `There was an error loading your blog posts`,
-  //     result.errors
-  //   )
-  //   return
-  // }
-
-  // const posts = result.data.allMarkdownRemark.nodes
-
-  // Create blog posts pages
-  // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
-  // `context` is available in the template as a prop and as a variable in GraphQL
-
-  // if (posts.length > 0) {
-  //   posts.forEach((post, index) => {
-  //     const previousPostId = index === 0 ? null : posts[index - 1].id
-  //     const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
-
-  //     createPage({
-  //       path: post.fields.slug,
-  //       component: blogPost,
-  //       context: {
-  //         id: post.id,
-  //         previousPostId,
-  //         nextPostId,
-  //       },
-  //     })
-  //   })
-  // }
+  
   const {createRedirect} = actions 
   createRedirect({  fromPath: `/temp/`,
   toPath: `/`,
@@ -69,6 +25,28 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   });
  
+}
+module.exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage, deletePage,createRedirect } = actions
+  // Check if the page is a localized 404
+  if (page.path.match(/^\/[a-z]{2}\/404\/$/)) {
+    const oldPage = { ...page }
+    // Get the language code from the path, and match all paths
+    // starting with this code (apart from other valid paths)
+    const langCode = page.path.split("/")[1]
+    page.matchPath = `/${langCode}/*`
+    // Recreate the modified page
+    deletePage(oldPage)
+    createPage(page)
+    createRedirect({
+      fromPath: `page`,
+      toPath:`page`,
+      statusCode: 404,
+      conditions: {
+        language: [`fr`],
+      },
+    });
+  }
 }
 
 /**
